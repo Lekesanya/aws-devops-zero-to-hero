065bbfeeb1f6f2885406c391aa051829321689f7
Lekesanya <155097176+Lekesanya@users.noreply.github.com>
2024-03-29 04:09:02 +0000

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push lekeadesanya/simple-python-flask-app:tagname
+docker push lekeadesanya/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 lekeadesanya/simple-python-flask-app
